8089fac65731eb43707f2cfb7240edf3b27c9079
Songyi Li <lisongyihaoren@163.com>
2016-04-03 09:32:36 +0800

    readme modified

--- dist/qq-wechat-emotion-parser.js
+++ dist/qq-wechat-emotion-parser.js
@@ -120,19 +120,19 @@ function build(){
 
 function qqWechatEmotionParser(str) {
     var indices = trie.search(str);
-    var increment = 0;
-    indices.map(function(idx) {
+    indices.reverse().map(function(idx) {
         var pos = idx[0],
             emotion = emotion_list[idx[1]],
             img = '<img src="' + emotion_map[emotion] + '" alt="' + emotion + '">';
-        str = splice(str, pos + increment, emotion.length, img);
-        increment = increment + img.length - emotion.length;
+        str = splice(str, pos, emotion.length, img);
     });
     return str;
 }
+
 function splice(str, index, count, add) {
     return str.slice(0, index) + add + str.slice(index + count);
 }
+
 function keys(map){
     var list = [];
     for (var k in map) {
